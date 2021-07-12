62328ad7d4bbf61c0e04b03042f557e78cc16630
cubeweek <cubeweek@gmail.com>
2021-07-13 01:16:01 +0900

    Chapter 03 JPA로 데이터다루기 완료!

--- src/main/java/com/drbaseball/sb/Application.java
+++ src/main/java/com/drbaseball/sb/Application.java
@@ -2,9 +2,10 @@ package com.drbaseball.sb;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 // 프로젝트의 메인 클래스
-// @SpringBootApplication 부터 스프링 부트의 자동 설정, Bean 읽기와 생성을 시작하므로 이 클래스는 항상 프로젝트의 최상단에 위치해야 함
+@EnableJpaAuditing
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
